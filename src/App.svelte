449347538e8dc17a9a5a6337d1f9c8255ac5c8da
olivierkra-jumiaCI <olivier.kra@jumia.com>
2026-04-27 16:54:46 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,33 @@
 <script>
+  import { onMount } from 'svelte';
   import JumiaDelivery from './JumiaDelivery.svelte'
   import JumiaBusiness from './JumiaBusiness.svelte'
   import JumiaRates from './JumiaRates.svelte'
   
   let currentView = 'personal';
 
-  function setView(view) {
-    currentView = view;
+  function handleHash() {
+    const hash = window.location.hash;
+    if (hash === '#/business') {
+      currentView = 'business';
+    } else if (hash === '#/tarifs') {
+      currentView = 'rates';
+    } else {
+      currentView = 'personal';
+    }
     window.scrollTo(0, 0);
+  }
+
+  onMount(() => {
+    handleHash();
+    window.addEventListener('hashchange', handleHash);
+    return () => window.removeEventListener('hashchange', handleHash);
+  });
+
+  function setView(view) {
+    if (view === 'personal') window.location.hash = '#/';
+    else if (view === 'business') window.location.hash = '#/business';
+    else if (view === 'rates') window.location.hash = '#/tarifs';
   }
 </script>
 
